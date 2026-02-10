ef96bd8542ac7385e67456e8e483f4496aa694b6
Augusto M.P <augusto.pinheiro@botpress.com>
2026-02-10 09:39:29 -0500

    feat: added nvim-tree-preview

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -20,9 +20,34 @@ return {
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    {
+      "b0o/nvim-tree-preview.lua",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+    },
   },
   config = function()
+    local api = require('nvim-tree.api')
+
+    local function on_attach(bufnr)
+      api.config.mappings.default_on_attach(bufnr)
+
+      local function tree_opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      local preview = require('nvim-tree-preview')
+
+      vim.keymap.set('n', 'P', preview.watch, tree_opts('Preview (Watch)'))
+      vim.keymap.set('n', '<Esc>', preview.unwatch, tree_opts('Close Preview/Unwatch'))
+      vim.keymap.set('n', '<C-f>', function() return preview.scroll(4) end, tree_opts('Scroll Down'))
+      vim.keymap.set('n', '<C-b>', function() return preview.scroll(-4) end, tree_opts('Scroll Up'))
+      vim.keymap.set('n', '<Tab>', preview.node_under_cursor, tree_opts('Preview'))
+    end
+
     require("nvim-tree").setup({
+      on_attach = on_attach,
       sync_root_with_cwd = true,
       view = {
         adaptive_size = true,
